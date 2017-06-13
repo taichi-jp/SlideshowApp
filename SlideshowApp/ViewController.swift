ecfe541af960d9cc0e688c7e7a78187916b3d7d4
taichikoumoto <sbskomoto@gmail.com>
2017-06-13 18:14:42 +0900

    訂正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     var count: Int = 0
     //再生中かどうか管理する変数
     var isStart: Bool = false
+    //戻る/進むボタン
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
     //再生/停止ボタンOutlet
     @IBOutlet weak var startButton: UIButton!
     //タイマー
@@ -49,6 +52,11 @@ class ViewController: UIViewController {
     //再生/停止ボタンAction
     @IBAction func start(_ sender: Any) {
         if isStart == false{//停止の時
+            
+            //訂正
+            backButton.isEnabled = false
+            forwardButton.isEnabled = false
+            
             startButton.setTitle("停止", for: .normal)
             self.timer = timer ?? Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(goNext), userInfo: nil, repeats: true)
             isStart = true
@@ -57,6 +65,10 @@ class ViewController: UIViewController {
             self.timer.invalidate()
             self.timer = nil
             isStart = false
+            
+            //訂正
+            backButton.isEnabled = true
+            forwardButton.isEnabled = true
         }
     }
     
@@ -74,6 +86,14 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let detailViewController:DetailViewController = segue.destination as! DetailViewController
         detailViewController.image = list[count]
+        
+        //訂正
+        startButton.setTitle("再生", for: .normal)
+        self.timer.invalidate()
+        self.timer = nil
+        backButton.isEnabled = true
+        forwardButton.isEnabled = true
+        isStart = false
     }
     
     //遷移先から戻る
